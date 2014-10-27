6ddf42d5cf36d59caffd5f6ea8fb59e3f7b0d3d5
Qinhu Wang <qinhu.wang@icloud.com>
2014-10-27 22:01:43 +0800

    add t/sRNA ratio distribution

--- lib/draw_distribution.r
+++ lib/draw_distribution.r
@@ -1,11 +1,16 @@
-pdf(file="distribution.pdf", 6, 6)
+pdf(file="distribution.pdf", 6, 9)
 # read length file
 rls<-read.table("srna.len")
 rlt<-read.table("trna.len")
+rld[1:length(rls$V1),1]<-rls$V1
+rld[1:length(rls$V1),2]<-rlt$V2
+rld[1:length(rls$V1),3]<-rls$V2 - rlt$V2
 # plot
-layout(c(1,2))
+layout(c(1:3))
 par(mar=(c(4.5,5,2,1)))
 barplot(rls$V2, col=2, main="Length distribution of sRNA reads", xlab="length (nt)", ylab="Frequency", names.arg=rls$V1)
 par(mar=(c(4.5,5,2,1)))
 barplot(rlt$V2, col=3, main="Length distribution of tRNA reads", xlab="length (nt)", ylab="Frequency", names.arg=rlt$V1)
+par(mar=(c(4.5,5,2,1)))
+barplot(t(rld), col=c("red","green"), main="Length distribution of tRNA and non-tRNA reads", xlab="length (nt)", ylab="Frequency", names.arg=rlt$V1)
 dev.off()
